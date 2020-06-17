2660e44e1ebd651fc16fe47ae6a2ac946ff5d898
Farhan Hasan <farhan.design@outlook.com>
2020-06-17 22:17:43 +0500

    setup graphql

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
-
-// To add a schema:
-// buildSchema function takes a string  and that string
-// should define our schema
 const { buildSchema } = require('graphql');
-const mongoose = require('mongoose');
-
-const Event = require('./models/event');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
-
-// schema: should point at a valid GraphQl Schema
-// rootValue: will point at a JS Object which has all
-// the resolver functions in it.
-// --> query: for fetching data
-//--> and mutation: for changing data
-// -- In type RootQuery we will define different endpoints
-//    we support for incoming queries.
-//--> In type RootMutation we will define our diff mutations
 app.use('/graphql', graphqlHttp({
   schema: buildSchema(`
     type Event {
@@ -55,48 +41,21 @@ app.use('/graphql', graphqlHttp({
   `),
   rootValue: {
     events: () => {
-      return Event.find()
-        .then(events => {
-          return events.map(event => {
-            return { ...event._doc, _id: event.id };
-          });
-        }).catch(err => {
-          throw err;
-        });
+      return events;
     },
     createEvent: (args) => {
-      const event = new Event({
+      const event = {
+        _id: Math.random().toString(),
         title: args.eventInput.title,
         description: args.eventInput.description,
         price: +args.eventInput.price,
-        date: new Date(args.eventInput.date)
-      });
+        date: args.eventInput.date
+      };
+      events.push(event);
       return event
-        .save()
-        .then(result => {
-          console.log(result);
-          return { ...result._doc, _id: result._doc._id.toString() };
-        })
-        .catch(err => {
-          console.log(err);
-          throw err;
-        });
     }
   },
   graphiql: true
 }));
 
-
-// .connect method needs a connection string which
-// is the address of our database cluster
-mongoose.connect(
-  `mongodb+srv://${process.env.MONGO_USER}:${
-    process.env.MONGO_PASSWORD
-  }@cluster0-mafhi.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-).then(() => {
-  app.listen(3500);
-}).catch(err => {
-  console.log(err);
-});
-
-
+app.listen(3500);
