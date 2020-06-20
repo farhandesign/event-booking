946e5c2af2962ed4e40afbc510792d33c9c49c2f
Farhan Hasan <farhan.design@outlook.com>
2020-06-20 19:32:25 +0500

    Graphql + mongoDb fixed error

--- app.js
+++ app.js
@@ -2,60 +2,88 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
 
-const events = [];
-
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
-  schema: buildSchema(`
-    type Event {
-      _id: ID!
-      title: String!
-      description: String!
-      price: Float!
-      date: String!
-    }
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
 
-    input EventInput {
-      title: String!
-      description: String!
-      price: Float!
-      date: String!
-    }
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
 
-    type RootQuery {
-      events: [Event!]!
-    }
+        type RootQuery {
+            events: [Event!]!
+        }
 
-    type RootMutation {
-      createEvent(eventInput: EventInput): Event
-    }
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
 
-    schema {
-      query: RootQuery
-      mutation: RootMutation
-    }
-  `),
-  rootValue: {
-    events: () => {
-      return events;
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      }
     },
-    createEvent: (args) => {
-      const event = {
-        _id: Math.random().toString(),
-        title: args.eventInput.title,
-        description: args.eventInput.description,
-        price: +args.eventInput.price,
-        date: args.eventInput.date
-      };
-      events.push(event);
-      return event
-    }
-  },
-  graphiql: true
-}));
+    graphiql: true
+  })
+);
 
-app.listen(3500);
+mongoose.connect(`mongodb+srv://farhan_design:${process.env.MONGO_PASSWORD}@cluster0.mafhi.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+})
+  .then(() => {
+    app.listen(3500);
+  })
+  .catch(err => console.log(err));
+
+
